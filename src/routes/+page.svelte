d6f6e62f7f3393b2062728762916dc396de68102
Gustav Ljungberg <gustav.ljungberg@gmail.com>
2024-03-29 11:59:43 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	const test = ['hej', 'hejsan'];
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each test as thing}
+	<h1>{thing}</h1>
+	<p>weee</p>
+{/each}
