842b702055aa37080bfc7ba111cc9d165db46145
goof bug <gurra2004@gmail.com>
2025-05-24 17:14:45 +0200

    Update to not use blob urls and image elements

--- src/routes/explode/+page.svelte
+++ src/routes/explode/+page.svelte
@@ -4,57 +4,56 @@
 
 	const gifWorkerUrl = new URL('gif.js/dist/gif.worker.js', import.meta.url);
 
+	let canvas: HTMLCanvasElement | undefined;
+
 	let files: FileList | undefined = $state();
 
 	let gifPromise = $derived.by(async () => {
-		if (files && files.length) {
-			const temporaryObjectUrls = [];
-			try {
-				const gif = new GIF({
-					width: 498,
-					height: 460,
-					debug: true,
-					workerScript: gifWorkerUrl.toString(),
-					quality: 1
-				});
-				for (const file of files) {
-					const img = new Image();
-					const temporaryObjectUrl = URL.createObjectURL(file);
-					temporaryObjectUrls.push(temporaryObjectUrl);
-					img.src = temporaryObjectUrl;
-					await img.decode();
-					gif.addFrame(img, { delay: 1000 });
-				}
-				for (const frame of frames) {
-					const img = new Image();
-					img.src = frame;
-					await img.decode();
-					gif.addFrame(img, { delay: 10 });
-				}
-				const { promise, resolve, reject } = Promise.withResolvers<Blob>();
-				gif.on('finished', (blob) => {
-					resolve(blob);
-				});
-				gif.on('abort', () => {
-					reject(new Error('Aborted'));
-				});
-
-				gif.render();
-
-				const blob = await promise;
-				return URL.createObjectURL(blob);
-			} finally {
-				for (const temporaryObjectUrl of temporaryObjectUrls) {
-					URL.revokeObjectURL(temporaryObjectUrl);
-				}
+		if (files && files.length && canvas) {
+			const ctx = canvas.getContext('2d');
+			if (!ctx) {
+				throw new Error('No canvas context');
 			}
+
+			const gif = new GIF({
+				width: 498,
+				height: 460,
+				debug: true,
+				workerScript: gifWorkerUrl.toString(),
+				quality: 1
+			});
+			for (const file of files) {
+				const image = await window.createImageBitmap(file);
+				ctx.drawImage(image, 0, 0, 498, 460);
+				gif.addFrame(ctx, { delay: 1000, copy: true });
+			}
+			for (const frame of frames) {
+				const img = new Image();
+				img.src = frame;
+				await img.decode();
+				gif.addFrame(img, { delay: 10 });
+			}
+			const { promise, resolve, reject } = Promise.withResolvers<Blob>();
+			gif.on('finished', (blob) => {
+				resolve(blob);
+			});
+			gif.on('abort', () => {
+				reject(new Error('Aborted'));
+			});
+
+			gif.render();
+
+			const blob = await promise;
+			return URL.createObjectURL(blob);
 		}
 	});
 </script>
 
 <div class="container">
-	<h1>Explode</h1>
+	<h1>Explode him</h1>
 	<input type="file" bind:files accept="image/*" />
+
+	<canvas class="hidden" bind:this={canvas} width="498" height="460"></canvas>
 
 	{#await gifPromise}
 		<p>Loading...</p>
@@ -72,6 +71,10 @@
 </div>
 
 <style>
+	.hidden {
+		display: none;
+	}
+
 	.container {
 		display: flex;
 		flex-direction: column;
